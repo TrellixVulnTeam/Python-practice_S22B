2ce49545c0fe1de50a0f16637464cc4b9b5341cc
VlzZ <vlzz1221@gmail.com>
2018-01-18 17:50:56 +0800

    string code

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-""" module docstring"""
+""" hello """
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
@@ -8,9 +8,29 @@ print('hello, world')
 # print(input())
 
 # 基本类型
-# 整数
-A = 100
-if A >= 0:
-    print(A)
-else:
-    print(-A)
+
+# 整数    1，100，-8080，0
+#   十六进制    0xff00，0xa5b4c3d2
+
+# 浮点数   1.23，3.14，-9.01
+#   科学计数法   1.23e9, 12.3e8, 1.2e-5
+
+# 字符串   'abc', "abc", "I'm OK", 'I\'m \"OK\"!', \t \n \\
+
+print('I\'m \"Ok!\"')
+print('I\'m ok.')
+print('I\'m learning\nPython.')
+print('\\\n\\')
+print('\\\t\\')
+print(r'\\\t\\')
+print('''line1
+    line2
+    line3''')
+
+PI = 3.14159265359
+print(PI)
+
+print(10 / 3)
+print(9 / 3)
+print(10 // 3)
+print(10 % 3)
---
+++ the_string.py
@@ -0,0 +1,36 @@
+""" hello """
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# print(ord('A'))
+# print(bin(ord('A')))
+# print(chr(65))
+
+# print(str(int('0b100000000', 2)))
+
+# print(ord('A'))
+# print(ord('中'))
+# print(chr(66))
+# print(chr(25991))
+
+# print('\u4e2d\u6587')
+
+# print('ABC'.encode('ascii'))
+# print('中文'.encode('utf-8'))
+
+# print(b'ABC'.decode('ascii'))
+# print(b'\xe4\xb8\xad\xe6\x96\x87'.decode('utf-8'))
+
+# print('Hello, %s' % 'world')
+# 占位符	替换内容
+# %d	整数
+# %f	浮点数
+# %s	字符串
+# %x	十六进制整数
+# print('Hi, %s, you have $%f.' % ('Michael', 1000000))
+
+# print('growth rate: %d %%' % 7)
+
+# print("Hello, {0}, 成绩提升了 {1:.1f}%".format('小明', 17.123))
+
+print('中文'.encode('gb2312'))

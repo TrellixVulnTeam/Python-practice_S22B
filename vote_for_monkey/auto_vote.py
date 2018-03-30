7c3d174b7aa839b2696cce59443c57d0cc98413e
VlzZ <vlzz1221@gmail.com>
2018-03-30 11:14:07 +0800

    vote_for_monkey

--- vote_for_monkey/auto_vote.py
+++ vote_for_monkey/auto_vote.py
@@ -1,26 +1,32 @@
 # -*- coding: utf-8 -*-
 
-import urllib2
+import http
+import time
 import urllib
-import cookielib
+from http import cookiejar
+from urllib import request
+
+
+def vote():
+    pre_url = r'http://promo.unitechance.com/picc2018contest/detial/40?from=groupmessage&isappinstalled=0'
+    url = r'http://promo.unitechance.com/api/picc2018contest/vote?id=40'
+
+    cookie = http.cookiejar.CookieJar()  # 声明一个CookieJar对象实例来保存cookie
+    handler = urllib.request.HTTPCookieProcessor(cookie)  # 利用urllib2库的HTTPCookieProcessor对象来创建cookie处理器
+    opener = urllib.request.build_opener(handler)  # 通过handler来构建opener
+
+    response = opener.open(pre_url)  # 此处的open方法同urllib2的urlopen方法，也可以传入request
+    time.sleep(1)
+    response = opener.open(url)
+    print(response.read().decode())
 
 
 def auto_vote():
+    while True:
+        print('start ...')
+        vote()
+        print('end ...')
+        time.sleep(1)
 
-    pre_url = 'http://promo.unitechance.com/picc2018contest/detial/40?from=groupmessage&isappinstalled=0'
-    url = 'http://promo.unitechance.com/api/picc2018contest/vote?id=40'
 
-    req = urllib2.Request(pre_url)
-    # 将cookie与opener绑定，原因是让两次请求用相同cookie
-    cj = cookielib.CookieJar()
-    opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
-
-    # 第一次请求
-    response = opener.open(req)
-
-    # 第二次请求
-    req = urllib2.Request(url)
-    response = opener.open(req)
-    html = response.read().decode('utf-8')
-    # 输出页面
-    print(html)
+auto_vote()

2bde999f7289f6e99dfc6c98301f368dc265eba5
VlzZ <vlzz1221@gmail.com>
2018-05-11 18:34:02 +0800

    socket

---
+++ basic/socket/do_server.py
@@ -0,0 +1,44 @@
+#!/user/bin/env python3
+# -*- coding: utf-8 -*-
+
+import socket
+import threading
+import time
+
+# TCP
+# def tcplink(sock, addr):
+#     print('Accept new connection from %s:%s...' % addr)
+#     sock.send(b'Welcome!')
+#     while True:
+#         data = sock.recv(1024)
+#         time.sleep(1)
+#         if not data or data.decode('utf-8') == 'exit':
+#             break
+#         sock.send(('Hello, %s!' % data.decode('utf-8')).encode('utf-8'))
+#     sock.close()
+#     print('Connection from %s:%s closed.' % addr)
+
+
+# s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# s.bind(('127.0.0.1', 9999))
+# s.listen(5)
+
+# print('Waiting for connection...')
+
+# while True:
+#     sock, addr = s.accept()
+#     t = threading.Thread(target=tcplink, args=(sock, addr))
+#     t.start()
+
+# UDP
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# 绑定端口:
+s.bind(('127.0.0.1', 9999))
+# 创建Socket时，SOCK_DGRAM指定了这个Socket的类型是UDP。绑定端口和TCP一样，但是不需要调用listen()方法，而是直接接收来自任何客户端的数据：
+
+print('Bind UDP on 9999...')
+while True:
+    # 接收数据:
+    data, addr = s.recvfrom(1024)
+    print('Received from %s:%s.' % addr)
+    s.sendto(b'Hello, %s!' % data, addr)

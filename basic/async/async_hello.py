538df1d44e2312a3292bff0e1661e9868722eca3
VlzZ <vlzz1221@gmail.com>
2018-06-26 18:41:53 +0800

    async

--- basic/async/async_hello.py
+++ basic/async/async_hello.py
@@ -1,14 +1,39 @@
 import asyncio
+import threading
+import time
+
 
 @asyncio.coroutine
 def hello():
     print("Hello world!")
     # 异步调用asyncio.sleep(1):
-    r = yield from asyncio.sleep(1)
+    r = yield from asyncio.sleep(10)
     print("Hello again!")
+
 
 # 获取EventLoop:
 loop = asyncio.get_event_loop()
 # 执行coroutine
-loop.run_until_complete(hello())
+tasks = [hello(), hello()]
+loop.run_until_complete(asyncio.wait(tasks))
 loop.close()
+
+
+# @asyncio.coroutine
+# def hello():
+#     print('Hello world! (%s)' % threading.currentThread())
+#     yield from asyncio.sleep(2)
+#     print('Hello again! (%s)' % threading.currentThread())
+
+
+# @asyncio.coroutine
+# def bye():
+#     print('Bye bye world! (%s)' % threading.currentThread())
+#     yield from asyncio.sleep(4)
+#     print('Bye bye again! (%s)' % threading.currentThread())
+
+
+# loop = asyncio.get_event_loop()
+# tasks = [bye(), hello()]
+# loop.run_until_complete(asyncio.wait(tasks))
+# loop.close()
--- basic/async/coroutine.py
+++ basic/async/coroutine.py
@@ -1,24 +1,131 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-def consumer():
-    r = ''
-    while True:
-        n = yield r
-        if not n:
-            return
-        print('[CONSUMER] Consuming %s...' % n)
-        r = '200 OK'
+# def consumer():
+#     r = 'here'
+#     while True:
+#         n = yield r
+#         if not n:
+#             return
+#         print('[CONSUMER] Consuming %s...' % n)
+#         r = '200 OK'
 
-def produce(c):
-    c.send(None)
-    n = 0
-    while n < 5:
-        n = n + 1
-        print('[PRODUCER] Producing %s...' % n)
-        r = c.send(n)
-        print('[PRODUCER] Consumer return: %s' % r)
-    c.close()
+# def produce(c):
+#     r = c.send(None)
+#     print(r)
+#     n = 0
+#     while n < 5:
+#         n = n + 1
+#         print('[PRODUCER] Producing %s...' % n)
+#         r = c.send(n)
+#         print('[PRODUCER] Consumer return: %s' % r)
+#     c.close()
 
-c = consumer()
-produce(c)
+# c = consumer()
+# produce(c)
+
+# import time
+# import asyncio
+
+
+# def now(): return time.time()
+
+
+# async def do_some_thing(x):
+#     print('Waiting: ', x)
+
+# # print(now())
+
+# loop = asyncio.get_event_loop()
+# # loop.run_until_complete()
+# # print(time.time())
+
+# # coroutine = do_some_thing('x')
+# # task = loop.create_task(coroutine)
+# tasks = [do_some_thing(2)]
+# # print(task)
+# loop.run_until_complete(asyncio.wait(tasks))
+
+
+# ============================================================
+
+import time
+import asyncio
+import functools
+
+
+# def now(): return time.time()
+
+
+# async def do_some_work(x):
+#     print('Waiting: ', x)
+#     return 'Done after {}s'.format(x)
+
+
+# def callback(future):
+#     print('Callback: ', future.result())
+
+
+# def callback_again(t, future):
+#     print('Callback:', t, future.result())
+
+
+# start = now()
+
+# coroutine = do_some_work(2)
+# loop = asyncio.get_event_loop()
+# task = asyncio.ensure_future(coroutine)
+# task.add_done_callback(callback)
+# task.add_done_callback(functools.partial(callback_again, 2))
+# loop.run_until_complete(task)
+
+# async def do_some_work(x):
+#     print('Waiting {}'.format(x))
+#     return 'Done after {}s'.format(x)
+
+# start = now()
+
+# coroutine = do_some_work(2)
+# loop = asyncio.get_event_loop()
+# # task = asyncio.ensure_future(coroutine)
+# task = loop.create_task(coroutine)
+# loop.run_until_complete(task)
+
+# print('Task ret: {}'.format(task.result()))
+# print('TIME: {}'.format(now() - start))
+
+# async def do_some_work(x):
+#     print('Waiting: ', x)
+ 
+#     await asyncio.sleep(x)
+#     return 'Done after {}s'.format(x)
+ 
+# start = now()
+ 
+# coroutine1 = do_some_work(1)
+# coroutine2 = do_some_work(2)
+# coroutine3 = do_some_work(4)
+ 
+# # tasks = [
+# #     asyncio.ensure_future(coroutine1),
+# #     asyncio.ensure_future(coroutine2),
+# #     asyncio.ensure_future(coroutine3)
+# # ]
+ 
+# tasks = [coroutine1, coroutine2, coroutine3]
+# # 这种 没有 result
+
+# loop = asyncio.get_event_loop()
+# loop.run_until_complete(asyncio.wait(tasks))
+ 
+# for task in tasks:
+#     print('Task ret: ', task.result())
+ 
+# print('TIME: ', now() - start)
+
+def next_id():
+    return '%1d000' % int(time.time() * 1000)
+
+
+idd = next_id()
+print(idd)
